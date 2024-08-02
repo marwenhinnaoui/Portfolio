de643664c447878a3e1f53501f309667844a46fe
marwen.hinaoui <admin@colis.tn>
2024-08-02 19:07:55 +0100

    sd

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import React from "react";
-import SectionHeading from "./section-heading";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+
 import "react-vertical-timeline-component/style.min.css";
-import { experiencesData } from "@/lib/data";
-import { useSectionInView } from "@/lib/hooks";
+
 import { useTheme } from "@/context/theme-context";
 
 export default function Experience() {
