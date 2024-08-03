3d9ae101b54287a90f95037c7690a0dbd6de7cf6
marwen.hinaoui <admin@colis.tn>
2024-08-03 11:09:22 +0100

    SD

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "marwen-hinaoui.tn",
+        hostname: "drive.google.com",
       },
     ],
+  },
+  experimental: {
+    serverActions: true,
   },
 };
 
