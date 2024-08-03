ead53e5248125824b527a3d280e13e4e234a9dac
marwen.hinaoui <admin@colis.tn>
2024-08-03 17:32:19 +0100

    s

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2024 Venky. All rights reserved.
+        &copy; 2024. All rights reserved.
       </small>
 
     </footer>
